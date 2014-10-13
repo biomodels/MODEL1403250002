8fa641cfc38c607a3d93be777f6c6d708204b0ad
Stanley Gu <stanleygu@gmail.com>
2014-10-13 06:49:05 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+    
+    
+    This is the reduced model corresponding to "glucose upshift" condition described in the paper "Testing Biochemistry Revisited: How In Vivo Metabolism Can Be Understood from In Vitro Enzyme Kinetics" by van Eunen et al published in Plos Comput Biol, 2012.
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='MODEL1403250002',
+      version=20140916,
+      description='MODEL1403250002 from BioModels',
+      url='http://www.ebi.ac.uk/biomodels-main/MODEL1403250002',
+      maintainer='Stanley Gu',
+      maintainer_url='stanleygu@gmail.com',
+      packages=find_packages(),
+      package_data={'': ['*.xml', 'README.md']},
+    )
